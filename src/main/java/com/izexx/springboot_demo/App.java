232ee7db9a0fa34bbb37e6c5807a61dce28f9721
izexx <izexx@outlook.com>
2019-10-31 16:53:51 +0800

    create project first time

---
+++ src/main/java/com/izexx/springboot_demo/App.java
@@ -0,0 +1,13 @@
+package com.izexx.springboot_demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// adds @Configuration, @EnableAutoConfiguration, @ComponentScan
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
